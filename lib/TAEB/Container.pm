84fb44a3a7b9de9a8c4838f1bae86374a751c296
Jesse Luehrs <doy@tozt.net>
2009-11-08 03:42:51 -0600

    clean up the taeb container a bit more

--- lib/TAEB/Container.pm
+++ lib/TAEB/Container.pm
@@ -6,8 +6,36 @@ use Cwd 'abs_path';
 use File::Spec;
 use File::HomeDir;
 use Log::Dispatch::Null;
-use TAEB::VT;
+use TAEB::Config;
 use TAEB::Logger;
+use TAEB::VT;
+
+has config_file => (
+    is      => 'ro',
+    isa     => 'Str',
+    lazy    => 1,
+    default => 'config.yml',
+);
+
+has config_path => (
+    is      => 'ro',
+    isa     => 'Str',
+    lazy    => 1,
+    builder => '_build_config_path',
+);
+
+has config => (
+    is      => 'ro',
+    isa     => 'TAEB::Config',
+    lazy    => 1,
+    default => sub {
+        my $self = shift;
+        TAEB::Config->new(
+            config_path => $self->config_path,
+            config_file => $self->config_file,
+        );
+    },
+);
 
 has container => (
     is      => 'ro',
@@ -15,7 +43,7 @@ has container => (
     builder => '_build_container',
 );
 
-sub _config_path {
+sub _build_config_path {
     $ENV{TAEBDIR} ||= do {
         File::Spec->catdir(File::HomeDir->my_home, '.taeb');
     };
@@ -31,55 +59,24 @@ sub _config_path {
 
 sub _build_container {
     my $self = shift;
+    my $config = $self->config;
+    my $container = $self;
+
     container 'TAEB' => as {
-        service config_file => 'config.yml';
-        service config_path => $self->_config_path;
-        service config => (
-            class        => 'TAEB::Config',
-            dependencies => [
-                depends_on('config_file'),
-                depends_on('config_path'),
-            ],
-        );
-
-        service interface => (
-            block => sub {
-                my $s = shift;
-                my $config = $s->param('config');
-
-                my $interface_config = $config->get_interface_config;
-                return $config->get_interface_class->new($interface_config);
-            },
-            dependencies => [
-                depends_on('config'),
-            ],
-        );
-
-        service scraper => (
-            class => 'TAEB::ScreenScraper',
-        );
-
-        service vt => (
-            block => sub {
-                my $vt = TAEB::VT->new(cols => 80, rows => 24);
-                $vt->option_set(LINEWRAP => 1);
-                $vt->option_set(LFTOCRLF => 1);
-                return $vt;
-            },
-        );
-
+        # XXX: need to make this an attribute on TAEB::Logger
+        service log_dir => $config->taebdir_file('log');
         service log => (
             block => sub {
                 my $s = shift;
-                my $display = $s->param('display');
 
-                my $log = TAEB::Logger->new;
+                my $log = TAEB::Logger->new(log_dir => $s->param('log_dir'));
                 $log->add_as_default(Log::Dispatch::Null->new(
                     name => 'taeb-warning',
                     min_level => 'warning',
                     max_level => 'warning',
                     callbacks => sub {
                         my %args = @_;
+                        my $display = $container->fetch('Display/display')->get;
                         # XXX: do we need to test for definedness?
                         if (!$display->to_screen) {
                             local $SIG{__WARN__};
@@ -92,6 +89,7 @@ sub _build_container {
                     min_level => 'error',
                     callbacks => sub {
                         my %args = @_;
+                        my $display = $container->fetch('Display/display')->get;
                         # XXX: do we need to test for definedness?
                         if (!$display->to_screen) {
                             local $SIG{__WARN__};
@@ -107,76 +105,118 @@ sub _build_container {
                 $log->setup_handlers;
                 return $log;
             },
-            dependencies => [
-                depends_on('display'),
-            ],
+            lifecycle    => 'Singleton',
+            dependencies => wire_names(qw(log_dir)),
+        );
+
+        container 'Interface' => as {
+            service class     => $config->get_interface_class;
+            service config    => $config->get_interface_config;
+            service interface => (
+                block => sub {
+                    my $s = shift;
+                    my $class = $s->param('class');
+                    return $class->new(
+                        config => $s->param('config'),
+                        log    => $s->param('log'),
+                    );
+                },
+                lifecycle    => 'Singleton',
+                dependencies => wire_names(qw(class config log)),
+            );
+        };
+
+        container 'Display' => as {
+            service class   => $config->get_display_class;
+            service config  => $config->get_interface_config;
+            service display => (
+                block => sub {
+                    my $s = shift;
+                    my $class = $s->param('class');
+                    return $class->new(
+                        config => $s->param('config'),
+                        log    => $s->param('log'),
+                    );
+                },
+                lifecycle    => 'Singleton',
+                dependencies => wire_names(qw(class config log)),
+            );
+        };
+
+        container 'AI' => as {
+            service class  => $config->get_ai_class;
+            service config => $config->get_ai_config;
+            service ai     => ( # persistent
+                block => sub {
+                    my $s = shift;
+                    my $class = $s->param('class');
+                    return $class->new(
+                        config => $s->param('config'),
+                        log    => $s->param('log'),
+                    );
+                },
+                lifecycle    => 'Singleton',
+                dependencies => wire_names(qw(class config log)),
+            );
+        };
+
+        service vt => (
+            block => sub {
+                my $s = shift;
+                my $vt = TAEB::VT->new(
+                    cols => 80,
+                    rows => 24,
+                    log  => $s->param('log')
+                );
+                $vt->option_set(LINEWRAP => 1);
+                $vt->option_set(LFTOCRLF => 1);
+                return $vt;
+            },
+            dependencies => wire_names(qw(log)),
+        );
+
+        service scraper => (
+            class => 'TAEB::ScreenScraper',
+            dependencies => wire_names(qw(log)),
         );
 
         service publisher => (
             class => 'TAEB::Publisher',
+            dependencies => wire_names(qw(log)),
         );
 
         service debugger => (
             class => 'TAEB::Debug',
-        );
-
-        service display => (
-            block => sub {
-                my $s = shift;
-                my $config = $s->param('config');
-
-                return $config->get_display_class->new;
-            },
-            dependencies => [
-                depends_on('config'),
-            ],
-        );
-
-        service ai => ( # persistent
-            block => sub {
-                my $s = shift;
-                my $config = $s->param('config');
-
-                return $config->get_ai_class->new;
-            },
-            dependencies => [
-                depends_on('config'),
-            ],
+            dependencies => wire_names(qw(log)),
         );
 
         service dungeon => ( # persistent
             class => 'TAEB::World::Dungeon',
+            dependencies => wire_names(qw(log)),
         );
 
         service senses => ( # persistent
             class => 'TAEB::Senses',
+            dependencies => wire_names(qw(log)),
         );
 
         service spells => ( # persistent
             class => 'TAEB::World::Spells',
+            dependencies => wire_names(qw(log)),
         );
 
         service item_pool => ( # persistent
             class => 'TAEB::World::ItemPool',
+            dependencies => wire_names(qw(log)),
         );
 
         service app => (
             class => 'TAEB',
-            dependencies => [
-                depends_on('config'),
-                depends_on('interface'),
-                depends_on('scraper'),
-                depends_on('vt'),
-                depends_on('log'),
-                depends_on('publisher'),
-                depends_on('debugger'),
-                depends_on('display'),
-                depends_on('ai'),
-                depends_on('dungeon'),
-                depends_on('senses'),
-                depends_on('spells'),
-                depends_on('item_pool'),
-            ],
+            dependencies => wire_names(qw(
+                log
+                Interface/interface Display/display AI/ai
+                vt scraper publisher debugger dungeon senses spells item_pool
+            )),
         );
     };
 }
